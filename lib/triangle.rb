297e32ccae0f8ed3914319b8a0f317b6675f76c2
Gregory Losco <gtlosco@gmail.com>
2019-12-15 17:30:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,6 +12,10 @@ class Triangle
   
   def valid_triangle
     triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a,b,c].each do |side|
+      triangle << false if side <= 0
+    end 
+    raise TriangleError if triangle.include?(false)
   end 
   
   class TriangleError < StandardError
